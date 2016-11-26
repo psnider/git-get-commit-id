4318a1227ade9212aef3a4b8c3e9822bd76546c3
Peter Snider <snider.peter@gmail.com>
2016-11-26 05:13:00 -0800

    use npm @types ; switch to @sabbatical

--- git-get-commit-id.js
+++ git-get-commit-id.js
@@ -1,26 +1,26 @@
 "use strict";
-var child_process = require('child_process');
+const child_process = require('child_process');
 function getGitStatus(done) {
     var cmd = 'git status --porcelain';
-    child_process.exec(cmd, function (error, stdout, stderr) {
+    child_process.exec(cmd, (error, stdout, stderr) => {
         if (error) {
-            error.message = "FAILED " + cmd + "\n" + error.message;
+            error.message = `FAILED ${cmd}\n${error.message}`;
         }
         var is_clean = !((stdout.length > 0) || (stderr.length > 0));
-        done(error, { stdout: stdout, stderr: stderr, is_clean: is_clean });
+        done(error, { stdout, stderr, is_clean });
     });
 }
 exports.getGitStatus = getGitStatus;
 function getGitCommitID(done) {
     var cmd = 'git rev-parse HEAD';
-    child_process.exec(cmd, function (error, stdout, stderr) {
+    child_process.exec(cmd, (error, stdout, stderr) => {
         if (stdout) {
             var id = stdout.toString().trim();
         }
         if (error) {
-            error.message = "FAILED " + cmd + "\n" + error.message;
+            error.message = `FAILED ${cmd}\n${error.message}`;
         }
-        done(error, { stdout: stdout, stderr: stderr, id: id });
+        done(error, { stdout, stderr, id });
     });
 }
 exports.getGitCommitID = getGitCommitID;
--- git-get-commit-id.tests.js
+++ git-get-commit-id.tests.js
@@ -1,11 +1,11 @@
 "use strict";
-var CHAI = require('chai');
-var expect = CHAI.expect;
-var git = require('./git-get-commit-id');
+const CHAI = require('chai');
+const expect = CHAI.expect;
+const git = require('./git-get-commit-id');
 describe('git-get-commit-id', function () {
     describe('getGitStatus', function () {
         it('should not return an error', function (done) {
-            git.getGitStatus(function (error, results) {
+            git.getGitStatus((error, results) => {
                 expect(error).to.not.exist;
                 done(error);
             });
@@ -13,7 +13,7 @@ describe('git-get-commit-id', function () {
     });
     describe('getGitCommitID', function () {
         it('should return a SHA1 commit-id for a git repo', function (done) {
-            git.getGitCommitID(function (error, results) {
+            git.getGitCommitID((error, results) => {
                 expect(results.id).to.match(/^[\da-fA-F]{40}$/);
                 done(error);
             });
