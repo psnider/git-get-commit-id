68a08de15efa557f70b61487156dc2a5ca4670f3
Peter Snider <snider.peter@gmail.com>
2017-01-20 13:54:54 -0800

    fix typings

--- git-get-commit-id.d.ts
+++ git-get-commit-id.d.ts
@@ -1,1 +1,2 @@
-export function getGitCommitID(done: (error: Error, stdout: string, stderr: string, id?: string) => void): void
+export function getGitStatus(done: (error: Error, result: {is_clean: boolean, stdout: string, stderr: string}) => void): void
+export function getGitCommitID(done: (error: Error, result: {id?: string, stdout: string, stderr: string}) => void): void
